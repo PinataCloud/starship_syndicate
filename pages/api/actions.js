2902599da660abdb9a8a63320d937b0bff6b8584
polluterofminds <justin.edward.hunter@gmail.com>
2023-09-23 19:37:42 -0500

    storing private key and auth token

--- pages/api/actions.js
+++ pages/api/actions.js
@@ -2,48 +2,69 @@ const pinataSDK = require('@pinata/sdk');
 const pinata = new pinataSDK(process.env.PINATA_KEY, process.env.PINATA_SECRET);
 const FULL_URL = process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000/api/'
 
-export default async function () {
-  if(method === "POST") {
+
+//  @TODO create a secret that is passed in from cron request
+export default async function (req, res) {
+  if(req.method === "POST") {
     console.log(req.body.accountId);
-    console.log(cid);
+    console.log(req.body.cid);
+
+    const json = await fetch(`http://starship_syndicate.mypinata.cloud/ipfs/${req.body.cid}`)
+    console.log(json);
+    //  We need to get info about the agent
+    //  We need to inform AI of our agent's current situation (summarize?)
+    //  We need to inform AI of the game options and restrictions
+    //  We need AI to act
   } else {
     //  GET ALL Agent IDs from DB and loop through making rapid API calls to serverless function
     let hasMore = true;
     let pageOffset = 0;
 
-    const allFiles = [];
-
-    const filter = {
-      metadata: {
-        keyvalues: {
-          starshipAccounts: {
-              value: 'true',
-              op: 'eq'
-          }
-        }, 
-        pageLimit: 100, 
-        pageOffset
-      }
-    }
-
+    let allFiles = [];
+    
     while(hasMore) {
-      const files = await pinata.pinList(filter);
-      allFiles.concat(files);
-      pageOffset = pageOffset + 100;
-      if(files.length === 0) {
-        hasMore = false
-      }
+      try {
+
+        const filter = {
+          metadata: {
+            keyvalues: {
+              starshipAccounts: {
+                  value: 'true',
+                  op: 'eq'
+              }
+            }
+          }, 
+          pageLimit: 100, 
+          pageOffset
+        }
+        console.log(filter)
+        const files = await pinata.pinList(filter);
+        console.log(files.rows.length);
+        allFiles = [...allFiles, ...files.rows];
+        pageOffset = pageOffset + 100;
+        if(files.rows.length === 0) {
+          hasMore = false
+        } 
+
+        res.json(allFiles);
+      } catch (error) {
+        console.log("Error in the while loop")
+        console.log(error);
+        throw error;
+      }      
     }  
     
-    files.forEach(file => {
+    console.log({allFiles});
+
+    allFiles.forEach(file => {
       fetch(`${FULL_URL}/actions`, {
         method: "POST", 
         headers: {
           'Content-Type': 'application/json'
         }, 
         body: JSON.stringify({
-          accountId: file.metadata.accountId, 
-          cid: file.ipfs_pin_has
+          accountId: file.metadata.keyvalues.accountId, 
+          cid: file.ipfs_pin_hash
         })
       })
     })
